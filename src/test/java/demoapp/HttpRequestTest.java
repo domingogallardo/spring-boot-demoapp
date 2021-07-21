34d0feb3c91c27d6579254ac6a2f00aa2f6b627d
Domingo Gallardo <domingo.gallardo@ua.es>
2021-07-21 10:26:14 +0200

    Actualizado a Spring Boot 2.4.8 y JUnit 5

--- src/test/java/demoapp/HttpRequestTest.java
+++ src/test/java/demoapp/HttpRequestTest.java
@@ -1,30 +1,23 @@
 package demoapp;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("Hello World");
+        String body = restTemplate.getForObject("/", String.class);
+        assertThat(body.contains("Hello World"));
     }
 }
--- src/test/java/demoapp/WebMockTest.java
+++ src/test/java/demoapp/WebMockTest.java
@@ -1,24 +1,19 @@
 package demoapp;
 
-
 import demoapp.service.SaludoService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WebMockTest {
@@ -26,16 +21,24 @@ public class WebMockTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+
+    // Podemos también mockear el servicio
     @MockBean
     private SaludoService service;
 
     @Test
     public void greetingShouldReturnMessageFromService() throws Exception {
 
+        // Y especificar lo que debe devolver una llamada a uno de sus métodos
         when(service.saluda("Domingo")).thenReturn("Hola Mock Domingo");
 
         this.mockMvc.perform(get("/saludo/Domingo"))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hola Mock Domingo")));
     }
