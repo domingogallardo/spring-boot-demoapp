df6021deb33e3e4a506a133b30e7e4032d55d97c
Domingo Gallardo <domingo.gallardo@ua.es>
2022-08-31 12:34:06 +0200

    Actualizados los tests

--- src/test/java/demoapp/MockServiceTest.java
+++ src/test/java/demoapp/MockServiceTest.java
@@ -11,22 +11,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class WebMockTest {
+public class MockServiceTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
-    }
 
     // Podemos también mockear el servicio
     @MockBean
